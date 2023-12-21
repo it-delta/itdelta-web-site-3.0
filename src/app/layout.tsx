79d4a0a07f382b8fddcea6d1735a0b9397befde1
Maxim Gerzon <gerzon@it-delta.ru>
2023-12-21 18:37:53 +0300

    components translated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import '@/styles/tailwind.css'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - Studio',
-    default: 'Studio - Award winning developer studio based in Denmark',
+    template: '%s - ITDELTA',
+    default: 'ITDELTA - Разработка программного обеспечения, IT консалтинг',
   },
 }
 
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,16 +11,16 @@ export default function NotFound() {
           404
         </p>
         <h1 className="mt-4 font-display text-2xl font-semibold text-neutral-950">
-          Page not found
+          Станица не найдена
         </h1>
         <p className="mt-2 text-sm text-neutral-600">
-          Sorry, we couldn’t find the page you’re looking for.
+          К сожалению мы не можем найти запрошенную страницу.
         </p>
         <Link
           href="/"
           className="mt-4 text-sm font-semibold text-neutral-950 transition hover:text-neutral-700"
         >
-          Go to the home page
+          На главную &rarr;
         </Link>
       </FadeIn>
     </Container>
