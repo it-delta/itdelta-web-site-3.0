634a5cd8cd436199c483d2a01ded93c1b6384146
Maxim Gerzon <gerzon@it-delta.ru>
2024-05-11 23:18:03 +0300

    yandex metrika updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,37 @@
-import { type Metadata } from 'next'
-import { Analytics } from '@/components/Analytics';
+import {type Metadata} from 'next'
+import {Analytics} from '@/components/Analytics';
 import Script from 'next/script';
-import { RootLayout } from '@/components/RootLayout'
+import {RootLayout} from '@/components/RootLayout'
 
 import '@/styles/tailwind.css'
 
 export const metadata: Metadata = {
-  title: {
-    template: '%s - ITDELTA',
-    default: 'ITDELTA - Разработка программного обеспечения, IT консалтинг',
-  },
-  other: {
-    'yandex-verification': '68030eb015a67d08',
-  }
+    title: {
+        template: '%s - ITDELTA',
+        default: 'ITDELTA - Разработка программного обеспечения, IT консалтинг',
+    },
+    other: {
+        'yandex-verification': '68030eb015a67d08',
+    }
 }
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
-      <Script src="https://code.jivo.ru/widget/PWVDc47MVr"/>
-      <body className="flex min-h-full flex-col">
-      <Analytics
-          tagID={96145741}
-          initParameters={{ clickmap: true, trackLinks: true, accurateTrackBounce: true }}
-      >
-          <RootLayout>{children}</RootLayout>
-      </Analytics>
-      </body>
-    </html>
-  )
+export default function Layout({children}: { children: React.ReactNode }) {
+    const prod = process.env.NODE_ENV === 'production';
+    return (
+        <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
+        <Script src="https://code.jivo.ru/widget/PWVDc47MVr"/>
+        <body className="flex min-h-full flex-col">
+        {prod ?
+            <Analytics
+                tagID={process.env.NEXT_PUBLIC_YANDEX_METRICA_ID ?? ''}
+                initParameters={{clickmap: true, trackLinks: true, accurateTrackBounce: true}}
+            >
+                <RootLayout>{children}</RootLayout>
+            </Analytics>
+            :
+            <RootLayout>{children}</RootLayout>
+        }
+        </body>
+        </html>
+    )
 }
