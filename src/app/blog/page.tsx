1a2f83e0ae67c36dbbf720f1092a7123448026e0
GRV <prog2@itdelta.dev>
2025-07-09 14:34:34 +0300

    feat(slider): created slider for the work images

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -68,6 +68,7 @@ export default async function Blog() {
                               alt=""
                               {...blog.author.image}
                               className="h-12 w-12 object-cover grayscale"
+                              unoptimized
                             />
                           </div>
                           <div className="text-sm text-neutral-950">
